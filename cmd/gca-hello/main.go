f9c05540b9397a066ecb6064164e7b4e6bcd1031
aweisser <armin.weisser@gmail.com>
2017-11-27 23:17:13 +0100

    Added simple cli parsing

--- cmd/gca-hello/main.go
+++ cmd/gca-hello/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/aweisser/clean-arch-golang/domain/gca"
 	"github.com/aweisser/clean-arch-golang/usecases/user"
 )
 
 func main() {
-	u := gca.User{Name: "Armin"}
+	var userName string
+	flag.StringVar(&userName, "user", "John Doe", "enter a user")
+	flag.Parse()
+	u := gca.User{Name: userName}
 	user.SayHello(u)
 }
