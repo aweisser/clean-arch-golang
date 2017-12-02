113121f61133e58a7774e47c9bc66344e7806a27
aweisser <armin.weisser@gmail.com>
2017-12-02 18:32:38 +0100

    Added tests for welcome.customer use case

--- usecase/welcome/customer_test.go
+++ usecase/welcome/customer_test.go
@@ -3,12 +3,27 @@ package welcome_test
 import (
 	"testing"
 
+	"github.com/aweisser/clean-arch-golang/domain/shop"
 	"github.com/aweisser/clean-arch-golang/usecase/welcome"
 
 	"github.com/aweisser/clean-arch-golang"
 
 	"github.com/stretchr/testify/assert"
 )
+
+type greeter struct {
+	welcomeClbck func(customer shop.Customer, message string)
+}
+
+func (g *greeter) Welcome(customer shop.Customer, message string) {
+	g.welcomeClbck(customer, message)
+}
+
+func TestOneCustomer(t *testing.T) {
+	factory.InitCliShop()
+	welcome.TheCustomer("John")
+	assert.Equal(t, 1, welcome.Counter.CountCustomers(), "There should only be one customers")
+}
 
 func TestTwoCustomers(t *testing.T) {
 	factory.InitCliShop()
@@ -17,7 +32,20 @@ func TestTwoCustomers(t *testing.T) {
 	assert.Equal(t, 2, welcome.Counter.CountCustomers(), "There should be two customers")
 }
 
-func TestUnique(t *testing.T) {
+func TestThereCanOnlyBeOneFirstCustomer(t *testing.T) {
+	factory.InitCliShop()
+	welcome.Greeter = &greeter{welcomeClbck: func(customer shop.Customer, message string) {
+		if customer.Name == "John" {
+			assert.Contains(t, message, "You're our first customer.")
+		} else {
+			assert.NotContains(t, message, "You're our first customer.")
+		}
+	}}
+	welcome.TheCustomer("John")
+	welcome.TheCustomer("Lisa")
+}
+
+func TestEachCustomerIsUnique(t *testing.T) {
 	factory.InitCliShop()
 	welcome.TheCustomer("John")
 	welcome.TheCustomer("John")
